0acc62cdcdc698a57cb225921ec4bb213ca815d2
Gaurav Kholiya <shinumishra000@gmail.com>
2022-12-02 15:30:14 +0530

    add multiply.c

---
+++ multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node
+{
+
+} s1;
+int main()
+{
+    int a = 9;
+    int b = 8;
+    int c = a + b;
+    printf("the sum is :%d", c);
+    return 0;
+}
